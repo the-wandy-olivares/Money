d560f8067348037456bba6ea3f2f186d6da171d1
Wandy Olivares <user.email>
2025-02-22 18:18:25 -0400

    Actualizar el dashboard para mostrar el conteo de clientes; modificar la plantilla y agregar lógica en la vista.

--- Easy/Easy/views.py
+++ Easy/Easy/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import  TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
+from Client import models
 
 
 class Dashboard(TemplateView):
     template_name = "dashboard/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cont_clients'] = models.Client.objects.all().count()
+        return context
