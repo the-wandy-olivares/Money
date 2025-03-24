d17c4b21d800b474e221223c1188722bb878599c
Wandy Olivares <user.email>
2025-03-24 00:41:07 -0400

    Agregar funcionalidad para actualizar inversiones y agregar fondos: implementar vistas, formularios y URL para gestionar la actualización de inversiones y la adición de fondos, mejorando la experiencia del usuario.

--- Easy/Inversion/urls.py
+++ Easy/Inversion/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "inversion"
 urlpatterns = [
       path('', views.Inversion.as_view(), name='inversion'),
-            path('create/', views.InversionCreate.as_view(),
-                  name='inversion-create'),
-
+            path('create/', views.InversionCreate.as_view(),  name='inversion-create'),
+                  path('update/<int:pk>/', views.InversionUpdate.as_view(), name='inversion-update'),
+            path('inversion-agregar-fondos/<int:pk>', views.InversionAgregarFondos.as_view(), name='inversion-agregar-fondos'),
 ]
--- Easy/Inversion/views.py
+++ Easy/Inversion/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, CreateView, UpdateView, DetailView, DeleteView 
 from django.urls import reverse_lazy
 from . import models 
 from . import forms
@@ -10,10 +10,15 @@ class Inversion(TemplateView):
       
       def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
-            inversions = models.Inversion.objects.filter(is_active=True).last()
+            inversions = models.Inversion.objects.filter(is_active=True).order_by('-pk').first()
             context['inversions'] = models.Inversion.objects.filter(is_active=True)
-            context['total_inversion'] = inversions.mount_inversion
-            context['retorno_inversion'] = inversions.mount_retorno 
+            if inversions:
+                context['total_inversion'] = inversions.mount_inversion
+                context['retorno_inversion'] = inversions.mount_retorno
+            else:
+                context['total_inversion'] = 0
+                context['retorno_inversion'] = 0
+            context['inversion_actual'] = inversions
             return context
       
       def get(self, request, *args, **kwargs):
@@ -40,3 +45,46 @@ class InversionCreate(CreateView):
       def get_success_url(self):
             return reverse_lazy('inversion:inversion')
 
+
+
+class InversionUpdate(UpdateView):
+      template_name = "inversion/inversion-update.html"
+      model = models.Inversion
+      form_class = forms.Inversion
+      
+      def form_valid(self, form):
+            form.instance.user = self.request.user
+            form.instance.mount_disponible = form.instance.mount_inversion
+            form.instance.company = self.request.user.more.company
+            form.save()
+            return super().form_valid(form)
+      
+      def form_invalid(self, form):
+            print(form.errors)
+            return super().form_invalid(form)
+      
+      def get_success_url(self):
+            return reverse_lazy('inversion:inversion')
+
+
+class InversionAgregarFondos(DetailView):
+      model = models.Inversion
+      template_name = 'inversion/inversion-agregar-fondos.html'
+      context_object_name = 'inversion'
+      
+      def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            inversion = self.get_object()
+            context['inversion'] = inversion
+            return context
+      
+      def post(self, request, *args, **kwargs):
+            inversion = self.get_object()
+            mount = request.POST.get('mount').replace(',', '')
+            inversion.mount_inversion += int(mount)
+            inversion.mount_disponible  += int(mount) 
+            inversion.save()
+            return redirect('inversion:inversion')
+      
+      def get_success_url(self):
+            return reverse_lazy('inversion:inversion')
