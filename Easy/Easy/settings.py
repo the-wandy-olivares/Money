29601c8a574a6f7ce2956c298a8522100ee2d4bf
Wandy Olivares <user.email>
2025-04-04 20:31:48 -0400

    Correccion

--- Easy/Easy/settings.py
+++ Easy/Easy/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/olivares-finance/Money/Easy/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Easy', 'static'),  # Ajusta la ruta según tu estructura
 ]
